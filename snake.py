b4bfe610af219f9c993ba5bc57c185ddda6eca7d
SzymonM2006 <szymonmrozek191@gmail.com>
2023-10-22 21:11:38 +0200

    last commit correcting the changes and launching the game

--- snake.py
+++ snake.py
@@ -2,18 +2,18 @@ import tkinter as tk
 import random
 
 #Window
-snake = tk.Tk()
-snake.title("Snake Game")
-snake.geometry("400x400")
-canvas = tk.Canvas(snake, bg="black", width=400,height=400);
+snake_game = tk.Tk()
+snake_game.title("Snake Game")
+snake_game.geometry("400x400")
+canvas = tk.Canvas(snake_game, bg="black", width=400,height=400)
 canvas.pack()
 
 
-snake.mainloop()
+
  # początkowa pozycja i punkty
 snake = [(100, 100), (90,100), (80,100)]
-snake_direction = "right"
-points = (200,200)
+snake_direction = "Right"
+points = (200, 200)
 
 #funkcje pomocnicze
 def draw_snake():
@@ -23,8 +23,8 @@ def draw_snake():
         canvas.create_rectangle(x, y, x + 10, y + 10, fill="green", tag="snake")
 
 def draw_food():
-    x, y = draw_food
-    canvas.create_rectangle(x, y, x +10, y +10, fill="yellow")
+    x, y = food
+    canvas.create_oval(x, y, x +10, y +10, fill="yellow")
 
 
 def move_snake():
@@ -38,29 +38,31 @@ def move_snake():
         x -= 10
     else:
         x += 10
-snake.insert(0,(x, y))
-if snake[0] == food:
-    generate_food()
-else:
-    snake.pop()
 
-    if chcek_collision():
+    snake.insert(0, (x, y))
+    if snake[0] == food:
+        generate_food()
+    else:
+        snake.pop()
+
+    if check_collision():
         canvas.delete("all")
-        canvas.create_text(200, 200, text="Get better and try again")
+        canvas.create_text(200, 200, text="Game Over", fill="white", font=("Helvetica", 24))
     else:
         draw_snake()
         draw_food()
-        snake.after(100,move_snake)
+        snake_game.after(100, move_snake)
 
-        def generate_food():
-            global food
-            x = random.randint(0,39) * 10
-            y = random.randint(0,39) * 10
-            food = (x, y)
+def generate_food():
+    global food
+    x = random.randint(0, 39) * 10
+    y = random.randint(0, 39) * 10
+    food = (x, y)
+
 
 def check_collision():
-    x, y =snake[0]
-    if x < 0 or x >= 400 or y < 0 or y>= 400 or snake[0] in snake[1:]:
+    x, y = snake[0]
+    if x < 0 or x >= 400 or y < 0 or y >= 400 or snake[0] in snake[1:]:
         return True
     return False
 
@@ -75,6 +77,12 @@ def change_direction(event):
     elif event.keysym == "Left" and snake_direction!= "Right":
         snake_direction = "Left"
 
-snake.bind("<KeyPress>", change_direction)
+snake_game.bind("<KeyPress>", change_direction)
+
+#Rozpoczęcie gry
+generate_food()
+move_snake()
+
+snake_game.mainloop()
 
 
